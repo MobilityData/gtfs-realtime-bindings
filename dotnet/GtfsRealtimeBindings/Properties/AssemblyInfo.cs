f9f6deb8436cbb03869b274856b68d54b7f49b7c
Brian Ferris <bdferris@google.com>
2015-01-22 11:05:08 -0800

    Fill in project fields in AssemblyInfo.cs

--- dotnet/GtfsRealtimeBindings/Properties/AssemblyInfo.cs
+++ dotnet/GtfsRealtimeBindings/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GtfsRealtimeBindings")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides .NET classes generated from the GTFS-realtime Protocol Buffer specification.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Google Inc.")]
 [assembly: AssemblyProduct("GtfsRealtimeBindings")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
