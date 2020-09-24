96c10bb9533be7a0a860853b7094baf69f72fca7
Sean Barbeau <sjbarbeau@gmail.com>
2020-09-24 12:11:09 -0400

    chore: Add more assertions to Java unit test
    This is now the complete definition of the serialized vehicle position pb file

--- java/src/test/java/com/google/transit/realtime/GtfsRealtimeTest.java
+++ java/src/test/java/com/google/transit/realtime/GtfsRealtimeTest.java
@@ -15,16 +15,14 @@
  */
 package com.google.transit.realtime;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import com.google.transit.realtime.GtfsRealtime.FeedEntity;
+import com.google.transit.realtime.GtfsRealtime.FeedMessage;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.junit.Test;
-
-import com.google.transit.realtime.GtfsRealtime.FeedEntity;
-import com.google.transit.realtime.GtfsRealtime.FeedMessage;
+import static org.junit.Assert.*;
 
 /**
  * Unit-test for {@link GtfsRealtime} generated class.
@@ -35,9 +33,38 @@ public class GtfsRealtimeTest {
   public void testParseFrom() throws IOException {
     InputStream in = getClass().getResourceAsStream("vehicle_position.pb");
     FeedMessage feed = FeedMessage.parseFrom(in);
+
+    // Header
+    assertTrue(feed.hasHeader());
+    assertTrue(feed.getHeader().hasGtfsRealtimeVersion());
+    assertEquals("1.0", feed.getHeader().getGtfsRealtimeVersion());
+
+    // Entity
     assertEquals(1, feed.getEntityCount());
     FeedEntity entity = feed.getEntity(0);
     assertEquals(entity.getId(), "1");
     assertTrue(entity.hasVehicle());
+    assertTrue(entity.getVehicle().hasPosition());
+    assertTrue(entity.getVehicle().getPosition().hasLatitude());
+    assertEquals(47.0f, entity.getVehicle().getPosition().getLatitude(), .00000);
+    assertTrue(entity.getVehicle().getPosition().hasLongitude());
+    assertEquals(-122.0f, entity.getVehicle().getPosition().getLongitude(), .00000);
+    assertTrue(entity.getVehicle().hasVehicle());
+    assertEquals("1", entity.getVehicle().getVehicle().getId());
+    assertTrue(entity.getVehicle().hasTrip());
+    assertTrue(entity.getVehicle().getTrip().hasTripId());
+    assertEquals("t0", entity.getVehicle().getTrip().getTripId());
+
+    assertFalse(entity.getVehicle().hasCurrentStopSequence());
+    assertFalse(entity.getVehicle().hasStopId());
+    assertFalse(entity.getVehicle().hasCurrentStatus());
+    assertFalse(entity.getVehicle().hasTimestamp());
+    assertFalse(entity.getVehicle().hasCongestionLevel());
+    assertFalse(entity.getVehicle().hasOccupancyStatus());
+    assertFalse(entity.getVehicle().getTrip().hasDirectionId());
+    assertFalse(entity.getVehicle().getTrip().hasRouteId());
+    assertFalse(entity.getVehicle().getTrip().hasScheduleRelationship());
+    assertFalse(entity.getVehicle().getTrip().hasStartDate());
+    assertFalse(entity.getVehicle().getTrip().hasStartTime());
   }
 }
