e8ca48c50c60341e23f66cf4bd297ae7ae81156d
Sean Barbeau <sjbarbeau@gmail.com>
2020-10-29 18:01:24 -0400

    chore: Add test to write and read message in Java

--- java/src/test/java/com/google/transit/realtime/GtfsRealtimeTest.java
+++ java/src/test/java/com/google/transit/realtime/GtfsRealtimeTest.java
@@ -17,10 +17,10 @@ package com.google.transit.realtime;
 
 import com.google.transit.realtime.GtfsRealtime.FeedEntity;
 import com.google.transit.realtime.GtfsRealtime.FeedMessage;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -29,8 +29,114 @@ import static org.junit.Assert.*;
  */
 public class GtfsRealtimeTest {
 
+  static GtfsRealtime.FeedMessage.Builder feedMessageBuilder;
+  static GtfsRealtime.FeedEntity.Builder feedEntityBuilder;
+  static GtfsRealtime.FeedHeader.Builder feedHeaderBuilder;
+  static GtfsRealtime.VehiclePosition.Builder vehiclePositionBuilder;
+  static GtfsRealtime.TripDescriptor.Builder tripDescriptorBuilder;
+  static GtfsRealtime.VehicleDescriptor.Builder vehicleDescriptorBuilder;
+
+  public final static String ENTITY_ID = "TEST_ENTITY";
+
+  @BeforeClass
+  public static void setup() {
+    feedMessageBuilder = GtfsRealtime.FeedMessage.newBuilder();
+
+    feedHeaderBuilder = GtfsRealtime.FeedHeader.newBuilder();
+    feedHeaderBuilder.setGtfsRealtimeVersion("2.0");
+    feedMessageBuilder.setHeader(feedHeaderBuilder);
+
+    feedEntityBuilder = GtfsRealtime.FeedEntity.newBuilder();
+    feedEntityBuilder.setId(ENTITY_ID);
+
+    vehiclePositionBuilder = GtfsRealtime.VehiclePosition.newBuilder();
+    tripDescriptorBuilder = GtfsRealtime.TripDescriptor.newBuilder();
+    vehicleDescriptorBuilder = GtfsRealtime.VehicleDescriptor.newBuilder();
+  }
+
   @Test
-  public void testParseFrom() throws IOException {
+  public void testWriteAndReadVehiclePositions() throws IOException {
+    final String FILE_NAME = "test-write-vehicle-position.pb";
+
+    // Write vehicle positions to a file
+    tripDescriptorBuilder.setTripId("A");
+    vehicleDescriptorBuilder.setId("1");
+    GtfsRealtime.Position.Builder positionBuilder = GtfsRealtime.Position.newBuilder();
+    positionBuilder.setLatitude(50.0f);
+    positionBuilder.setLongitude(-60.0f);
+
+    vehiclePositionBuilder.setVehicle(vehicleDescriptorBuilder.build());
+    vehiclePositionBuilder.setTrip(tripDescriptorBuilder.build());
+    vehiclePositionBuilder.setPosition(positionBuilder.build());
+    feedEntityBuilder.setVehicle(vehiclePositionBuilder.build());
+    feedMessageBuilder.addEntity(0, feedEntityBuilder.build());
+
+    // Write and read vehicle positions to/from byte array
+    byte[] bytes = feedMessageBuilder.build().toByteArray();
+    validateParsedFeed(FeedMessage.parseFrom(bytes));
+
+    // Write and read vehicle positions to/from file
+    OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(FILE_NAME)));
+    feedMessageBuilder.build().writeTo(out);
+    out.close();
+    FileInputStream fis = new FileInputStream(FILE_NAME);
+    validateParsedFeed(FeedMessage.parseFrom(fis));
+
+    clearAndInitRequiredFeedFields();
+  }
+
+  private static void validateParsedFeed(FeedMessage feed) {
+    // Header
+    assertTrue(feed.hasHeader());
+    assertTrue(feed.getHeader().hasGtfsRealtimeVersion());
+    assertEquals("2.0", feed.getHeader().getGtfsRealtimeVersion());
+
+    // Entity
+    assertEquals(1, feed.getEntityCount());
+    FeedEntity entity = feed.getEntity(0);
+    assertEquals(entity.getId(), "TEST_ENTITY");
+    assertTrue(entity.hasVehicle());
+    assertTrue(entity.getVehicle().hasPosition());
+    assertTrue(entity.getVehicle().getPosition().hasLatitude());
+    assertEquals(50.0f, entity.getVehicle().getPosition().getLatitude(), .00000);
+    assertTrue(entity.getVehicle().getPosition().hasLongitude());
+    assertEquals(-60.0f, entity.getVehicle().getPosition().getLongitude(), .00000);
+    assertTrue(entity.getVehicle().hasVehicle());
+    assertEquals("1", entity.getVehicle().getVehicle().getId());
+    assertTrue(entity.getVehicle().hasTrip());
+    assertTrue(entity.getVehicle().getTrip().hasTripId());
+    assertEquals("A", entity.getVehicle().getTrip().getTripId());
+
+    assertFalse(entity.getVehicle().hasCurrentStopSequence());
+    assertFalse(entity.getVehicle().hasStopId());
+    assertFalse(entity.getVehicle().hasCurrentStatus());
+    assertFalse(entity.getVehicle().hasTimestamp());
+    assertFalse(entity.getVehicle().hasCongestionLevel());
+    assertFalse(entity.getVehicle().hasOccupancyStatus());
+    assertFalse(entity.getVehicle().getTrip().hasDirectionId());
+    assertFalse(entity.getVehicle().getTrip().hasRouteId());
+    assertFalse(entity.getVehicle().getTrip().hasScheduleRelationship());
+    assertFalse(entity.getVehicle().getTrip().hasStartDate());
+    assertFalse(entity.getVehicle().getTrip().hasStartTime());
+  }
+
+  private static void clearAndInitRequiredFeedFields() {
+    // clear the fields first
+    feedEntityBuilder.clear();
+    feedMessageBuilder.clear();
+    feedHeaderBuilder.clear();
+
+    vehiclePositionBuilder.clear();
+
+    feedHeaderBuilder.setGtfsRealtimeVersion("2.0");
+    feedMessageBuilder.setHeader(feedHeaderBuilder);
+
+    feedEntityBuilder.setId(ENTITY_ID);
+    feedMessageBuilder.addEntity(feedEntityBuilder);
+  }
+
+  @Test
+  public void testParseFromExistingFile() throws IOException {
     InputStream in = getClass().getResourceAsStream("vehicle_position.pb");
     FeedMessage feed = FeedMessage.parseFrom(in);
 
