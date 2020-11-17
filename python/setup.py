f6274de1ff2ef57b3bec95a6f2b1ab13616dfa7a
Jason Allen <jason.allen@itoworld.com>
2020-11-17 22:37:19 +0000

    Update python bindings to new proto release. Minor change in Python example to be Python3 syntax, not Python2

--- python/setup.py
+++ python/setup.py
@@ -15,37 +15,35 @@
 # limitations under the License.
 
 import sys
-import os
-import subprocess
 
 # We must use setuptools, not distutils, because we need to use the
 # namespace_packages option for the "google" package.
 try:
-  from setuptools import setup, Extension
+    from setuptools import setup, Extension
 except ImportError:
-  sys.stderr.write(
-    "Could not import setuptools; make sure you have setuptools or "
-    "ez_setup installed.\n")
-  raise
+    sys.stderr.write(
+        "Could not import setuptools; make sure you have setuptools or "
+        "ez_setup installed.\n")
+    raise
 
 if __name__ == '__main__':
-  ext_module_list = []
+    ext_module_list = []
 
-  setup(name = 'gtfs-realtime-bindings',
-        version = '0.0.6',
-        packages = ['google', 'google.transit'],
-        namespace_packages = ['google'],
-        install_requires = ['setuptools', 'protobuf'],
-        url = 'https://github.com/MobilityData/gtfs-realtime-bindings',
-        maintainer = 'MobilityData',
-        maintainer_email = 'gtfs-realtime@googlegroups.com',
-        license = 'Apache License, Version 2.0',
-        description = 'Python classes generated from the GTFS-realtime protocol buffer specification.',
-        classifiers=[
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules'
-        ],
-  )
+    setup(name='gtfs-realtime-bindings',
+          version='0.0.7',
+          packages=['google', 'google.transit'],
+          namespace_packages=['google'],
+          install_requires=['setuptools', 'protobuf'],
+          url='https://github.com/MobilityData/gtfs-realtime-bindings',
+          maintainer='MobilityData',
+          maintainer_email='gtfs-realtime@googlegroups.com',
+          license='Apache License, Version 2.0',
+          description='Python classes generated from the GTFS-realtime protocol buffer specification.',
+          classifiers=[
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: Apache Software License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+          )
